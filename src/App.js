ebb19b0ab20d66c1bcd95dcf646e2b86b09f3b59
aapodshivalov <aapodshivalov@gmail.com>
2021-07-01 14:01:44 +0400

    table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { data } from './data';
 
 function App() {
+  const getTable = (data) => {
+    data.filter(el => el.isActive); // фильтруем , берем активные строки
+    if (!data || data.length === 0) {
+      return <p>Нет данных</p>
+    }
+    return <table className="table">
+      <caption>Таблица</caption>
+      <tbody>
+        <tr>
+          <th>Баланс</th>
+          <th>Имя</th>
+          <th>Email</th>
+        </tr>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.balance}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  }
+
+  const table = getTable(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{table}</div>
     </div>
   );
 }
